4d4f42a8bbcedb77b3b5f650e8ea6ee0d53de303
Adel Khedhiri <khedhiri.adel@gmail.com>
2024-05-21 09:08:00 +0100

    first commit

--- src/App.js
+++ src/App.js
@@ -1,23 +1,56 @@
-import logo from './logo.svg';
+
+import { useState } from 'react';
 import './App.css';
 
 function App() {
+   const [firstName, setFirstName] = useState("");
+   const [lastName, setLastName] = useState("");
+   const [email, setEmail] = useState("");
+   const [password, setPassword] = useState("");
+   const [confirmPassword, setConfirmPassword] = useState("");
+   const handleSubmit = (e) => {
+    e.preventDefault();
+    const newUser = {firstName :firstName,
+                  lastName : lastName, 
+                  email: email,
+                  password: password,
+                  confirmPassword: confirmPassword
+        };
+
+        console.log(newUser);
+        setFirstName('')
+        setLastName('')
+        setEmail('')
+        setPassword('');
+        setConfirmPassword('');
+   };
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div className="container mt-5">
+      <form onSubmit ={handleSubmit}>
+          <div ClassName="mb-3">
+              <label className="form-label">First Name</label>
+              <input type="text" className="form-control" value={firstName} onChange={(e) => setFirstName(e.target.value)} />
+              <label className="form-label">Last Name</label>
+              <input type="text" className="form-control" value= {lastName} onChange={(e) => setLastName(e.target.value)} />
+              <label className="form-label">Email</label>
+              <input type="text" className="form-control" value={email} onChange={(e) => setEmail(e.target.value)} />
+              <label className="form-label">Password</label>
+              <input type="text" className="form-control" value={password} onChange={(e) => setPassword(e.target.value)} />
+              <label className="form-label">Confirm Password</label>
+              <input type="text" className="form-control" value={confirmPassword} onChange={(e) => setConfirmPassword(e.target.value)} />
+              <button>Create User</button>
+          </div>
+          <div ClassName = "container mt-5">
+              <p>First Name : {firstName}</p>
+              <p>Last Name : {lastName}</p>
+              <p>Email : {email}</p>
+              <p>Password : {password}</p>
+              <p>Confirm Password : {confirmPassword}</p>
+          </div>
+      </form>
+
+      
     </div>
   );
 }
